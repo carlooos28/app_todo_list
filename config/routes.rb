fd391939c754e54f4423a0d8c0c11f64d086f21f
Carlos Moreno <devcarlosmoreno@gmail.com>
2020-11-16 23:39:45 -0500

    new: Added config devise api JWT

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/health', to: 'health#health'
-
-  resources :posts, only: [:index, :show, :create, :update]
+  devise_for :users, skip: %i[registrations sessions passwords]
+  devise_scope :user do
+    post '/signup', to: 'registrations#create'
+    post '/login', to: 'sessions#create'
+    delete '/logout', to: 'sessions#destroy'
+  end
 end
